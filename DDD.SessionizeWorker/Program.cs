5a60c7cd5e6810c0f2de23a44a5a3179efa9c72a
Rob Moore <me@robdmoore.id.au>
2018-03-19 08:40:35 +0800

    Added File->New ASP.NET Core and Console App (Web Job)

---
+++ DDD.SessionizeWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDD.SessionizeWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
