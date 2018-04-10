95c58be77230a2cb58adf0cdac367806863c5d6d
Rob Moore <me@robdmoore.id.au>
2018-04-11 00:21:24 +0800

    Removing field that sessionize doesn't support

--- DDD.Core/Domain/Presenter.cs
+++ DDD.Core/Domain/Presenter.cs
@@ -7,7 +7,6 @@ namespace DDD.Core.Domain
         public Guid Id { get; set; }
         public string ExternalId { get; set; }
         public string Name { get; set; }
-        //public string Email { get; set; }
         public string Tagline { get; set; }
         public string Bio { get; set; }
         public string ProfilePhotoUrl { get; set; }
