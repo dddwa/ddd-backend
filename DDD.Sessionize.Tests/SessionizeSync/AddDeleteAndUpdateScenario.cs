ab00f9e1817f6ba8eeb11564cc7d6c2b61f97368
Mo Zaatar <mohamed.zaatar@gmail.com>
2019-04-16 18:14:26 +0800

    reduce test warnings

--- DDD.Sessionize.Tests/SessionizeSync/AddDeleteAndUpdateScenario.cs
+++ DDD.Sessionize.Tests/SessionizeSync/AddDeleteAndUpdateScenario.cs
@@ -11,82 +11,79 @@ using Xunit;
 using Xunit.Abstractions;
 using Scenario = DDD.Sessionize.Tests.TestHelpers.Scenario;
 
-namespace DDD.Sessionize.Tests.SessionizeSync
-{
-    public class AddDeleteAndUpdateScenario : Scenario
+namespace DDD.Sessionize.Tests.SessionizeSync {
+    public class AddDeleteAndUpdateScenario : Scenario 
     {
-        public void GivenEmptyReadModel()
-        {
-            _sessionRepository = new TableStorageRepositoryMock<SessionEntity>();
-            _presenterRepository = new TableStorageRepositoryMock<PresenterEntity>();
-        }
-
-        public void AndGivenSessionizeHasPresentersAndSessions()
-        {
-            _sessionizeApiClient = SessionizeApiClientMock.Get(
-                GetResource("EmptyReadModelScenarioMock.json"));
-        }
-
-        public async Task WhenPerformingSync()
-        {
-            await SyncService.Sync(_sessionizeApiClient, _sessionRepository, _presenterRepository,  _logger, _dateTimeProvider, "2018");
-        }
-
-        public void AndGivenSessionizeHasNewUpdatedAndDeletedPresentersAndSessions()
-        {
-            _sessionizeApiClient = SessionizeApiClientMock.Get(
-                GetResource("AddDeleteAndUpdateScenarioMock.json"));
-        }
-
-        public async Task WhenPerformingSubsequentSync()
-        {
-            _dateTimeProvider = new StaticDateTimeProvider(_dateTimeProvider.Now().AddDays(1));
-            await SyncService.Sync(_sessionizeApiClient, _sessionRepository, _presenterRepository, _logger, _dateTimeProvider, "2018");
-        }
-
-        public async Task ThenTheReadModelIsPopulated()
-        {
-            _readModel = (await _sessionRepository.GetAllAsync("2018")).ToArray();
-            _readModel.ShouldNotBeEmpty();
-        }
-
-        public async Task AndTheReadModelHasTheCorrectPresenters()
-        {
-            Approve(SessionOrPresenterAssertions.PreparePresentersForApproval((await _presenterRepository.GetAllAsync("2018")).ToArray()), "json");
-        }
-
-        public void AndTheReadModelHasTheCorrectSessions()
-        {
-            Approve(SessionOrPresenterAssertions.PrepareSessionsForApproval(_readModel), "json");
-        }
-
-        public void AndTheLoggerOutputIsCorrect()
-        {
-            Approve(_logger.ToString());
+        [Fact]
+        public void GivenEmptyReadModel () {
+            _sessionRepository = new TableStorageRepositoryMock<SessionEntity> ();
+            _presenterRepository = new TableStorageRepositoryMock<PresenterEntity> ();
         }
 
         [Fact]
-        public override void Run()
-        {
-            this.Given(x => x.GivenEmptyReadModel())
-                .And(x => x.AndGivenSessionizeHasPresentersAndSessions())
-                .When(x => x.WhenPerformingSync())
-                .Given(x => x.AndGivenSessionizeHasNewUpdatedAndDeletedPresentersAndSessions())
-                .When(x => x.WhenPerformingSubsequentSync())
-                .Then(x => x.ThenTheReadModelIsPopulated())
-                .And(x => x.AndTheReadModelHasTheCorrectPresenters())
-                .And(x => x.AndTheReadModelHasTheCorrectSessions())
-                .And(x => x.AndTheLoggerOutputIsCorrect())
-                .BDDfy(GetType().Name);
+        public void AndGivenSessionizeHasPresentersAndSessions () {
+            _sessionizeApiClient = SessionizeApiClientMock.Get (
+                GetResource ("EmptyReadModelScenarioMock.json"));
         }
 
-        public AddDeleteAndUpdateScenario(ITestOutputHelper output)
-        {
-            _logger = new Xunit2Logger(output);
-            Xunit2BddfyTextReporter.Instance.RegisterOutput(output);
+        [Fact]
+        public async Task WhenPerformingSync () {
+            await SyncService.Sync (_sessionizeApiClient, _sessionRepository, _presenterRepository, _logger, _dateTimeProvider, "2018");
         }
 
-        private StaticDateTimeProvider _dateTimeProvider = new StaticDateTimeProvider(new DateTimeOffset(2010, 1, 1, 0, 0, 0, TimeSpan.Zero));
+        [Fact]
+        public void AndGivenSessionizeHasNewUpdatedAndDeletedPresentersAndSessions () {
+            _sessionizeApiClient = SessionizeApiClientMock.Get (
+                GetResource ("AddDeleteAndUpdateScenarioMock.json"));
+        }
+
+        [Fact]
+        public async Task WhenPerformingSubsequentSync () {
+            _dateTimeProvider = new StaticDateTimeProvider (_dateTimeProvider.Now ().AddDays (1));
+            await SyncService.Sync (_sessionizeApiClient, _sessionRepository, _presenterRepository, _logger, _dateTimeProvider, "2018");
+        }
+
+        [Fact]
+        public async Task ThenTheReadModelIsPopulated () {
+            _readModel = (await _sessionRepository.GetAllAsync ("2018")).ToArray ();
+            _readModel.ShouldNotBeEmpty ();
+        }
+
+        [Fact]
+        public async Task AndTheReadModelHasTheCorrectPresenters () {
+            Approve (SessionOrPresenterAssertions.PreparePresentersForApproval ((await _presenterRepository.GetAllAsync ("2018")).ToArray ()), "json");
+        }
+
+        [Fact]
+        public void AndTheReadModelHasTheCorrectSessions () {
+            Approve (SessionOrPresenterAssertions.PrepareSessionsForApproval (_readModel), "json");
+        }
+
+        [Fact]
+        public void AndTheLoggerOutputIsCorrect () {
+            Approve (_logger.ToString ());
+        }
+
+        [Fact]
+        public override void Run () {
+            this.Given (x => x.GivenEmptyReadModel ())
+                .And (x => x.AndGivenSessionizeHasPresentersAndSessions ())
+                .When (x => x.WhenPerformingSync ())
+                .Given (x => x.AndGivenSessionizeHasNewUpdatedAndDeletedPresentersAndSessions ())
+                .When (x => x.WhenPerformingSubsequentSync ())
+                .Then (x => x.ThenTheReadModelIsPopulated ())
+                .And (x => x.AndTheReadModelHasTheCorrectPresenters ())
+                .And (x => x.AndTheReadModelHasTheCorrectSessions ())
+                .And (x => x.AndTheLoggerOutputIsCorrect ())
+                .BDDfy (GetType ().Name);
+        }
+
+        public AddDeleteAndUpdateScenario (ITestOutputHelper output) {
+            _logger = new Xunit2Logger (output);
+            Xunit2BddfyTextReporter.Instance.RegisterOutput (output);
+        }
+
+        private StaticDateTimeProvider _dateTimeProvider = new StaticDateTimeProvider (new DateTimeOffset (2010, 1, 1, 0, 0, 0, TimeSpan.Zero));
         private TableStorageRepositoryMock<SessionEntity> _sessionRepository;
         private TableStorageRepositoryMock<PresenterEntity> _presenterRepository;
         private ISessionizeApiClient _sessionizeApiClient;
