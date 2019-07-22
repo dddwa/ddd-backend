f6a8549eb6bf9c782811d0d0728cbcffdc704138
Andrew Best <andrew.best@readify.net>
2019-07-22 15:34:49 +0930

    Updates Deploy-Local to use new AzureRm Context apis

--- infrastructure/Deploy-Local.ps1
+++ infrastructure/Deploy-Local.ps1
@@ -11,10 +11,10 @@ $fields = ConvertFrom-Json ([System.IO.File]::ReadAllText($privateParamsFile))
 $fieldsAsHashTable = @{}
 $fields.PSObject.Properties | ForEach-Object { $fieldsAsHashTable[$_.Name] = $_.Value }
 
-$profile = Select-AzureRmProfile -Path "$PSScriptRoot\profile.json" -ErrorAction SilentlyContinue
+$profile = Import-AzureRmContext -Path "$PSScriptRoot\profile.json" -ErrorAction SilentlyContinue
 if (-not $profile.Context) {
-    Login-AzureRmAccount -TenantId $TenantId | Out-Null
-    Save-AzureRmProfile -Path "$PSScriptRoot\profile.json"
+    Login-AzureRmAccount -TenantId $fieldsAsHashTable['TenantId'] | Out-Null
+    Save-AzureRmContext -Path "$PSScriptRoot\profile.json"
 }
 
 & $PSScriptRoot\Deploy.ps1 -AlreadyLoggedIn @fieldsAsHashTable
