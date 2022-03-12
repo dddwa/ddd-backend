8b51c046af3db0eb309dc2aba471cf7130d478f7
Mo Zaatar <mohamed.zaatar@gmail.com>
2022-03-13 00:34:53 +0800

    chnage IsDraw to string to save it in db table (boolean is not saved)

--- DDD.Core/EloVoting/EloVote.cs
+++ DDD.Core/EloVoting/EloVote.cs
@@ -12,7 +12,7 @@ namespace DDD.Core.EloVoting
             RowKey = voteId.ToString();
             WinnerSessionId = winnerSessionId;
             LoserSessionId = loserSessionId;
-            IsDraw = isDraw;
+            IsDraw = isDraw.ToString();
             IpAddress = ipAddress;
             VoterSessionId = voterSessionId;
             VotingSubmittedTime = votingSubmittedTime;
@@ -20,7 +20,7 @@ namespace DDD.Core.EloVoting
 
         public string WinnerSessionId { get; set; }
         public string LoserSessionId { get; set; }
-        public bool IsDraw { get; private set; }
+        public string IsDraw { get; private set; }
         public string IpAddress { get; set; }
         public string VoterSessionId { get; set; }
         public DateTimeOffset VotingSubmittedTime { get; set; }
