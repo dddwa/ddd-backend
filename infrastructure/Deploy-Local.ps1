332e72f49ccf12e097e137e1030efbd976edc81c
Rob Moore <robm@dddperth.com>
2022-06-02 06:50:55 +0800

    Updated to PowerShell Core (#84)

--- infrastructure/Deploy-Local.ps1
+++ infrastructure/Deploy-Local.ps1
@@ -11,10 +11,4 @@ $fields = ConvertFrom-Json ([System.IO.File]::ReadAllText($privateParamsFile))
 $fieldsAsHashTable = @{}
 $fields.PSObject.Properties | ForEach-Object { $fieldsAsHashTable[$_.Name] = $_.Value }
 
-$profile = Import-AzureRmContext -Path "$PSScriptRoot\profile.json" -ErrorAction SilentlyContinue
-if (-not $profile.Context) {
-    Login-AzureRmAccount -TenantId $fieldsAsHashTable['TenantId'] | Out-Null
-    Save-AzureRmContext -Path "$PSScriptRoot\profile.json"
-}
-
-& $PSScriptRoot\Deploy.ps1 -AlreadyLoggedIn @fieldsAsHashTable
+& $PSScriptRoot\Deploy.ps1 @fieldsAsHashTable
